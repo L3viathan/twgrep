f506267e36ff59fbac7c6c5fec0caaa651ebdbac
L3viathan <git@l3vi.de>
2014-11-02 02:31:11 +0100

    removed 'TODO'

--- twgrep.py
+++ twgrep.py
@@ -95,7 +95,7 @@ for f in glob.glob("*.js"):
 				print("\nhttps://twitter.com/"+tweet["user"]["screen_name"]+"/status/"+tweet["id_str"]," – ",tweet["created_at"]+":\n"+tweet["text"])
 			elif args['-a']:
 				print(tweet)
-			elif args['--format']: #print custom format. TODO: make more pythonic.
+			elif args['--format']: #print custom format.
 				replacements = {
 					"?user": tweet["user"]["screen_name"],
 					"?text": tweet['text'],
